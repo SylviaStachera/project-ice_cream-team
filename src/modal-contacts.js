a0ef74f516c5b3bf649f04fc845c055b9cc14108
SzymonKomar <114801537+SzymonKomar@users.noreply.github.com>
2022-12-18 22:28:57 +0100

    feature
    Dodałem mapy (Natomiast są dość proste) oraz możliwość użycia przycisku feature do przejścia do strony na wikipedi franczyza

---
+++ src/modal-contacts.js
@@ -0,0 +1,15 @@
+import './sass/main.scss';
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[contacts-modal-open]'),
+    closeModalBtn: document.querySelector('[contacts-modal-close]'),
+    modal: document.querySelector('[contacts-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
